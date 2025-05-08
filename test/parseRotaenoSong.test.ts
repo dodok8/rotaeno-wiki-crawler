a9bbe4d133c30b4d633f0296d2701b16ec8313d3
Kim, Hyeonseo <dodok8@gmail.com>
2025-05-08 20:27:34 +0900

    test: add unit test for parsing Rotaeno song page 심장병병

---
+++ test/parseRotaenoSong.test.ts
@@ -0,0 +1,37 @@
+import { expect, test, describe } from 'bun:test'
+
+describe('Parse Rotaeno Song Page', () => {
+  test('심장병', async () => {
+    const document = await fetchRotaenoWikiPage('심장병')
+    const result = parseRotaenoSong(document)
+
+    expect(result).toEqual({
+      title: '심장병',
+      imageUrl:
+        'https://cdn.wikiwiki.jp/to/w/rotaeno/%EC%8B%AC%EC%9E%A5%EB%B3%91/::ref/sinz.png.webp',
+      pack: '基本曲',
+      composer: 'HyuN feat. HUBOG',
+      chartDesigner: 'TangScend',
+      artwork: 'Juice Black',
+      ver: '1.0.3',
+      charts: [
+        {
+          difficulty: 'Ⅰ',
+          const: 1.0,
+        },
+        {
+          difficulty: 'Ⅱ',
+          const: 3.0,
+        },
+        {
+          difficulty: 'Ⅲ',
+          const: 8.2,
+        },
+        {
+          difficultyLevel: 'Ⅳ',
+          const: 9.5,
+        },
+      ],
+    })
+  })
+})
