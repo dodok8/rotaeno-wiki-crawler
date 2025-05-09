128e22d10de7cbcbd7c6bee17bc881e9b6f85c13
Kim, Hyeonseo <dodok8@gmail.com>
2025-05-09 14:01:59 +0900

    test: add test for "Alfheim's Faith" song parsing

--- test/parseSong.test.ts
+++ test/parseSong.test.ts
@@ -37,4 +37,42 @@ describe('Parse Rotaeno Song Page', () => {
       ],
     })
   })
+  test("Alfheim's Faith", async () => {
+    const document = await fetchWikiPage("Alfheim's Faith")
+    const result = parseSong(document)
+
+    expect(result).toEqual({
+      title: "Alfheim's Faith",
+      id: "Alfheim's Faith-影虎。",
+      imageUrl:
+        'https://cdn.wikiwiki.jp/to/w/rotaeno/Alfheim%27s%20Faith/::ref/Alfeim%27s%20Faith_0.jpg.webp',
+      pack: '第3章 - 水と油',
+      composer: '影虎。',
+      chartDesigner: 'ⅠⅡⅢⅣ-α AXERA Ⅳ TPRT Wen',
+      artwork: '霜月',
+      ver: '2.0.0',
+      charts: [
+        {
+          difficulty: 'Ⅰ',
+          const: 4.0,
+        },
+        {
+          difficulty: 'Ⅱ',
+          const: 7.0,
+        },
+        {
+          difficulty: 'Ⅲ',
+          const: 10.5,
+        },
+        {
+          difficulty: 'Ⅳ',
+          const: 12.5,
+        },
+        {
+          difficulty: 'Ⅳ-α',
+          const: 13.2,
+        },
+      ],
+    })
+  })
 })
